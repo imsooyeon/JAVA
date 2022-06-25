721376c9f34043bc53cc1072ff9ba62ab6409cf9
Sooyeon Im <isyqwer1@gmail.com>
2022-06-25 22:18:26 +0900

    Update ex01.java

--- src/test2/ex01.java
+++ src/test2/ex01.java
@@ -4,6 +4,6 @@ public class ex01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello! This is eclipse-github practice");
+		System.out.println("Hello! World!");
 	}
 }
