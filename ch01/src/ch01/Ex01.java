9e2c302152dd2985b3f93c94d1462867d7070a6d
Sooyeon Im <isyqwer1@gmail.com>
2022-06-26 17:54:26 +0900

    wip

---
+++ ch01/src/ch01/Ex01.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ch01;
+
+/**
+ * @author imsooyeon
+ *
+ */
+public class Ex01 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, world.");
+	}
+
+}
