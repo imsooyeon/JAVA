3b28cd7c427df851aa13c6760864986333aa78d0
Sooyeon Im <isyqwer1@gmail.com>
2022-06-27 19:17:50 +0900

    Add: added StringEx.java

---
+++ ch02/src/ch02/StringEx.java
@@ -0,0 +1,24 @@
+package ch02;
+
+class StringEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "Ja" + "va";
+		String str = name + 8.0;
+		
+		System.out.println(name);       //Java
+		System.out.println(str);        //Java8.0
+		System.out.println(7 + " ");    //7 (String)
+		System.out.println(" " + 7);    // 7 (String)
+		System.out.println(7 + "");     //7 (String)
+		System.out.println("" + 7);     //7 (String)
+		System.out.println("" + "");    // (String)
+		System.out.println(7 + 7 + ""); //14 (String)
+		System.out.println("" + 7 + 7); //77 (String)
+		
+		//타입확인
+		//System.out.println(변수.getClass().getName());
+	}
+
+}
