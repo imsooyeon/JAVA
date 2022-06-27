46e8a29c7fd7c6bb7e6d28810820c2bb98ce0aa7
Sooyeon Im <isyqwer1@gmail.com>
2022-06-27 19:53:08 +0900

    modified StringEx.java

--- ch02/src/ch02/StringEx.java
+++ ch02/src/ch02/StringEx.java
@@ -15,7 +15,7 @@ class StringEx {
 		System.out.println("" + 7);     //7 (String)
 		System.out.println("" + "");    // (String)
 		System.out.println(7 + 7 + ""); //14 (String)
-		System.out.println("" + 7 + 7); //77 (String)
+		System.out.println("" + 7 + 8); //77 (String)
 		
 		//타입확인
 		//System.out.println(변수.getClass().getName());
