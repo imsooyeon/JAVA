bd794c31b9627f9bb3efd279d7eb46eb14001444
Sooyeon Im <isyqwer1@gmail.com>
2022-06-28 17:26:54 +0900

    Modify PrintfEx1.java

--- ch02/src/ch02/PrintfEx1.java
+++ ch02/src/ch02/PrintfEx1.java
@@ -17,11 +17,11 @@ class PrintfEx1 {
 		
 		System.out.printf("b=%d%n", b);  //b=1
 		System.out.printf("s=%d%n", s); //s=2
-		System.out.printf("c=%c, %d %n", c, (int)c); //c='A',A 10진수로 바꾼 
+		System.out.printf("c=%c, %d %n", c, (int)c); //c='A',아스키코드 A를 10진수로 바꾼 수 
 		System.out.printf("finger=[%5d]%n", finger); //finger=[   10]
 		System.out.printf("finger=[%-5d]%n", finger); //finger=[10   ] 
-		System.out.printf("finger=[%05d]%n", finger); //finger=[] 모르겠 finger=[00010]
-		System.out.printf("hex=%#x%n", hex); //hex= 모르겠
+		System.out.printf("finger=[%05d]%n", finger); //finger=[00010]
+		System.out.printf("hex=%#x%n", hex); //hex= 모르겠, -> %x, %o에 #를 사용하면 접두사 '0x'와 0이 각각 붙는다. #x 는 소문자 #X는 대문
 		System.out.printf("octNum=%o, %d%n", octNum, octNum); //octNum=10, octNum을 10진수로 바꾼 수 
 		System.out.printf("hexNum=%x, %d%n", hexNum, hexNum); //hexNum=10, hexNum을 10진수로 바꾼 수 
 		System.out.printf("binNum=%s, %d%n", Integer.toBinaryString(binNum), binNum); /*binNum=10, binNum을 10진수로 바꾼 수 , 
