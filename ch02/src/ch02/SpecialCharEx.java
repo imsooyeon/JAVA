811903177952973c5a90d82c2ceeed2883af3b33
Sooyeon Im <isyqwer1@gmail.com>
2022-06-28 20:42:53 +0900

    Add: added SpecialCharEx.java

---
+++ ch02/src/ch02/SpecialCharEx.java
@@ -0,0 +1,13 @@
+package ch02;
+
+class SpecialCharEx {
+
+	public static void main(String[] args) {
+		System.out.println('\''); //'
+		System.out.println("abc\t123\b456"); //abc	12456
+		System.out.println('\n'); //(new line)
+		System.out.println("\"Hello\""); //"Hello"
+		System.out.println("c:\\"); //??
+	}
+
+}
