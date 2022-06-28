51ac5718092c9c3d7a23221bd4e5336bada7e522
Sooyeon Im <isyqwer1@gmail.com>
2022-06-28 17:33:04 +0900

    Modify: modified PrintfEx1.java

--- ch02/src/ch02/PrintfEx1.java
+++ ch02/src/ch02/PrintfEx1.java
@@ -21,6 +21,7 @@ class PrintfEx1 {
 		System.out.printf("finger=[%5d]%n", finger); //finger=[   10]
 		System.out.printf("finger=[%-5d]%n", finger); //finger=[10   ] 
 		System.out.printf("finger=[%05d]%n", finger); //finger=[00010]
+		System.out.printf("big=%d%n", big); //big = 100000000000
 		System.out.printf("hex=%#x%n", hex); //hex= 모르겠, -> %x, %o에 #를 사용하면 접두사 '0x'와 0이 각각 붙는다. #x 는 소문자 #X는 대문
 		System.out.printf("octNum=%o, %d%n", octNum, octNum); //octNum=10, octNum을 10진수로 바꾼 수 
 		System.out.printf("hexNum=%x, %d%n", hexNum, hexNum); //hexNum=10, hexNum을 10진수로 바꾼 수 
