038f63c5020194c8c1031b34db7b3f8d1c3df3e5
Sooyeon Im <isyqwer1@gmail.com>
2022-06-29 13:40:59 +0900

    Modify: modified SpecialCharEx.java

--- ch02/src/ch02/SpecialCharEx.java
+++ ch02/src/ch02/SpecialCharEx.java
@@ -7,7 +7,7 @@ class SpecialCharEx {
 		System.out.println("abc\t123\b456"); //abc	12456
 		System.out.println('\n'); //(new line)
 		System.out.println("\"Hello\""); //"Hello"
-		System.out.println("c:\\"); //??
+		System.out.println("c:\\"); //?? -> c:\ \*바로 뒤의 문자를 일반 문자열로 인식하게 된다. → 자바에서 \ 작성 시에는 \ 를 한번 더 붙여 \\ 으로 작성해야 한다.*/
 	}
 
 }
