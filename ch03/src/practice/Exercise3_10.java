47bc680239dcfa7bec0ffe35a12dd1698f93548e
Sooyeon Im <isyqwer1@gmail.com>
2022-07-02 19:42:05 +0900

    연습문제 3-10 추가

---
+++ ch03/src/practice/Exercise3_10.java
@@ -0,0 +1,14 @@
+package practice;
+
+class Exercise3_10 {
+
+	public static void main(String[] args) {
+		char ch = 'A';
+//		char lowerCase = ('A'<=ch && ch<= 'Z') ? (ch+32) : ch;
+		char lowerCase = ('A'<=ch && ch<= 'Z') ? (char)(ch + 32) : ch;
+		System.out.println("ch:"+ch);
+		System.out.println("ch to lowerCase:"+lowerCase);
+	}
+/*  ch:A
+	ch to lowerCase:a*/
+}
