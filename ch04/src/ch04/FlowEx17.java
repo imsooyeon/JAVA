603cb14afcb1c55cfe888bce74903ac56bb318c2
Sooyeon Im <isyqwer1@gmail.com>
2022-07-04 09:59:16 +0900

    Add ex4-17

---
+++ ch04/src/ch04/FlowEx17.java
@@ -0,0 +1,24 @@
+package ch04;
+
+import java.util.*;
+
+class FlowEx17 {
+
+	public static void main(String[] args) {
+		int num = 0;
+		
+		System.out.print("*을 출력할 라인 수 입력:");
+		
+		Scanner scanner = new Scanner(System.in);
+		String tmp = scanner.nextLine();
+		num = Integer.parseInt(tmp);
+		
+		for(int i=0;i<num;i++) {
+			for(int j=0;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
