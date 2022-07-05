11a110875f0a63133ff1138c9d2eb8dc9c53bb2a
Sooyeon Im <isyqwer1@gmail.com>
2022-07-05 18:48:42 +0900

    Add: 배열 String을 char배열 변환 예제

---
+++ ch05/src/ch05/ArrayEx14.java
@@ -0,0 +1,18 @@
+package ch05;
+
+class ArrayEx14 {
+
+	public static void main(String[] args) {
+		String src = "ABCDE";
+		
+		for(int i=0; i< src.length(); i++) {
+			char ch = src.charAt(i);
+			System.out.println("src.charAt("+i+"):"+ ch);
+			
+			char[] chArr = src.toCharArray();  //String을 char[]로 변환 
+			
+			System.out.println(chArr);
+		}
+	}
+
+}
