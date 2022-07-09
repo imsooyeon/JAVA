c66ab79fd04c908f1cd0f9d78f405d104b5ef9e2
Sooyeon Im <isyqwer1@gmail.com>
2022-07-09 17:14:42 +0900

    연습문제 5-2 추가

---
+++ ch05/src/exercise/Exercise5_2.java
@@ -0,0 +1,16 @@
+package exercise;
+//arr[3].length의 값은 얼마인가 
+class Exercise5_2 {
+
+	public static void main(String[] args) {
+		int[][] arr = {
+				{ 5, 5, 5, 5, 5},
+				{ 10, 10, 10},
+				{ 20, 20, 20, 20},
+				{ 30, 30}  //arr[3]
+				};
+		
+		System.out.println(arr[3].length);
+	}
+
+}
