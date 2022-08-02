a6cfd57f419ae17843b8c19d4e881fc90c43bc7f
Sooyeon Im <isyqwer1@gmail.com>
2022-08-02 13:05:46 +0900

    연습문제 7-10, 7-11 추가

--- ch07/src/exercise/Exercise7_10.java
+++ ch07/src/exercise/Exercise7_10.java
@@ -9,6 +9,10 @@ class Exercise7_10 {
 		System.out.println("CH:"+t.getChannel());
 		t.setVolume(20);
 		System.out.println("VOL:"+t.getVolume());
+		t.gotoPrevChannel();
+		System.out.println("CH:"+t.getChannel());
+		t.gotoPrevChannel();
+		System.out.println("CH:"+t.getChannel());
 	}
 
 }
@@ -17,6 +21,7 @@ class MyTv2 {
 	private boolean isPowerOn;
 	private int channel;
 	private int volume;
+	private int prevChannel; //11번  
 	
 	final int MAX_VOLUME = 100;
 	final int MIN_VOLUME = 0;
@@ -27,12 +32,19 @@ class MyTv2 {
 	public void setChannel(int channel) {
 		if(channel > MAX_CHANNEL || channel < MIN_CHANNEL)
 			return;
+		
+		prevChannel = this.channel; //11번  
 		this.channel = channel;
 	}
+	
 	public int getVolume() {return volume;}
 	public void setVolume(int volume) {
 		if(volume > MAX_VOLUME || volume < MIN_VOLUME)
 			return;
 		this.volume = volume;
+	}
+	
+	public void gotoPrevChannel() {  //11번  
+		setChannel(prevChannel); //11번  
 	}
 }
