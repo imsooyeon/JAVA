1d33a47ac082e5a0989f295e0bf844a53e9f54d8
Sooyeon Im <isyqwer1@gmail.com>
2022-08-05 10:03:38 +0900

    Add: 예외처리 예외발생 예제

---
+++ ch08/src/ch08/ExceptionEx5.java
@@ -0,0 +1,25 @@
+package ch08;
+
+class ExceptionEx5 {
+
+	public static void main(String[] args) {
+		System.out.println(1);
+		System.out.println(2);
+		try {
+			System.out.println(3);
+			System.out.println(0/0);
+			System.out.println(4);
+		} catch (ArithmeticException ae) {
+			System.out.println(5);
+		}
+		System.out.println(6);
+	}
+
+}
+/*
+1
+2
+3
+5
+6
+*/
