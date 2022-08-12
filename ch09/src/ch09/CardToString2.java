81164fba17cd13f3a070451bc2c3b7eeeb828107
Sooyeon Im <isyqwer1@gmail.com>
2022-08-12 12:23:27 +0900

    Modify: toString메서드 오버라이딩 예제2

--- ch09/src/ch09/CardToString2.java
+++ ch09/src/ch09/CardToString2.java
@@ -21,8 +21,8 @@ class Card2 {
 
 class CardToString2 {
 	public static void main(String[] args) {
-		Card c1 = new Card();
-		Card c2 = new Card("HEART", 10);
+		Card2 c1 = new Card2();
+		Card2 c2 = new Card2("HEART", 10);
 		System.out.println(c1.toString());
 		System.out.println(c2.toString());
 	}
